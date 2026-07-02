7a4f838495e2ea04967527447830a3cc6a72ca23
Tyler Wu <tylerwu137@gmail.com>
2026-07-02 00:01:33 -0400

    fixed link routing

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -1,6 +1,5 @@
 import {Stack, Box, Typography, Button, Link} from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import { Link as RouterLink } from "react-router-dom";
 
 import SplashscreenSkeleton from "../components/SplashscreenSkeleton";
 import UserAccountTextField from "../components/UserAccountTextField";
@@ -33,7 +32,7 @@ export default function SignUpScreen() {
             <Typography variant="h2" sx={{color:"var(--brown)"}}>Sign Up</Typography>
             <Stack direction="row" spacing={0.5}>
               <Typography variant="caption" sx={{color: "var(--cream)"}}>Already have an account?</Typography>
-              <Link component={RouterLink} to="/login" variant="caption" sx={{color: "var(--cream)", textDecorationColor: "inherit"}}>Login here!</Link>
+              <Link component="button" onClick={() => navigate("/login")} variant="caption" sx={{color: "var(--cream)", textDecorationColor: "inherit"}}>Login here!</Link>
             </Stack>
           </Stack>
           <Stack spacing={2} sx={{width: "100%"}}>
